6a0e140ba95a96f31cd8e68964a8839d8efc831a
oliveiragustavo <gof@cin.ufpe.br>
2019-08-26 22:12:25 -0300

    testing integration with jenkins

--- db/models/index.js
+++ db/models/index.js
@@ -1,35 +1,30 @@
-// require('dotenv').config({ path: process.env.NODE_ENV || '.env' })
-// 'use strict';
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const basename = path.basename(__filename);
-// const config = require(__dirname + '/../../config/database.js');
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const config = require(__dirname + '/../../config/database.js');
 const db = {};
 
-// console.log('----------')
-// console.log(config)
-// console.log('----------')
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-// let sequelize = new Sequelize(config.database, config.username, config.password, config);
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//   })
-//   .forEach(file => {
-//     const model = sequelize['import'](path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
