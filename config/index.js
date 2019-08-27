95d9c34523a68688e5134c20faf78d2f76e60bc9
oliveiragustavo <gof@cin.ufpe.br>
2019-08-27 00:16:15 -0300

    testing integration with jenkins

--- config/index.js
+++ config/index.js
@@ -1,9 +1,5 @@
 require('dotenv').config({ path: process.env.NODE_ENV || '.env' })
 
-console.log("========")
-console.log(process.env.DBUSERNAME)
-console.log("========")
-
 module.exports = {
   secret: process.env.SECRET,
   port: process.env.PORT,
